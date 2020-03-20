710e64ee9dbd2c3bd63d990dcd3eab4a493f5113
nikkieyabut <201801011@iacademy.edu.ph>
2020-03-20 11:27:06 +0800

    testing

--- src/server/src/resolvers.js
+++ src/server/src/resolvers.js
@@ -1,8 +1,23 @@
 // TODO: Import Mongoose here
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true})
+
+
 // TODO: Connect to mongo here
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+  // we're connected!
+})
+
+var movieSchema = new mongoose.Schema({
+  title: String
+  // type: String,
+  // filename: String
+})
 
 // TODO: Replace `{}` with actual Movie model
-const Movie = {}
+const Movie = mongoose.model('Movie', movieSchema)
 
 const getMovieList = async () => {
   return await Movie.find({})
